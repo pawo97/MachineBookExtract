fd9110fb7cae2ce8c4cadaed149742cae12ca3eb
p.wolski@polsource.com <pa-wo97@o2.pl>
2021-03-13 17:13:46 +0100

    Add nltk

--- MachineBookExtract/Main.py
+++ MachineBookExtract/Main.py
@@ -1,1 +1,28 @@
-print("Welcome to procject")
+import nltk
+
+text = "Bed and chair are types of furniture"
+
+print(text)
+#
+# DT is the determinant
+#
+# VBP is the verb
+#
+# JJ is the adjective
+#
+# IN is the preposition
+#
+# NN is the noun
+
+sentence = [("a", "DT"),("clever","JJ"),("fox","NN"),("was","VBP"),
+   ("jumping","VBP"),("over","IN"),("the","DT"),("wall","NN")]
+
+grammar = "NP:{<DT>?<JJ>*<NN>}"
+
+parser_chunking = nltk.RegexpParser(grammar)
+
+parser_chunking.parse(sentence)
+
+output = parser_chunking.parse(sentence)
+
+output.draw()
