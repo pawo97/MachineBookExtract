ba50dd0b700854c0536f8f9dfab0879b5a2ffddc
p.wolski@polsource.com <pa-wo97@o2.pl>
2021-03-20 16:25:16 +0100

    Initial book

--- MachineBookExtract/Main.py
+++ MachineBookExtract/Main.py
@@ -1,28 +1,91 @@
-import nltk
+# import nltk
 
-text = "Bed and chair are types of furniture"
+# extract epub to text:
+# https://github.com/kevinxiong/epub2txt
 
-print(text)
 #
-# DT is the determinant
+# text = "Bed and chair are types of furniture"
 #
-# VBP is the verb
+# print(text)
+# #
+# # DT is the determinant
+# #
+# # VBP is the verb
+# #
+# # JJ is the adjective
+# #
+# # IN is the preposition
+# #
+# # NN is the noun
 #
-# JJ is the adjective
+# sentence = [("a", "DT"),("clever","JJ"),("fox","NN"),("was","VBP"),
+#    ("jumping","VBP"),("over","IN"),("the","DT"),("wall","NN")]
 #
-# IN is the preposition
+# grammar = "NP:{<DT>?<JJ>*<NN>}"
 #
-# NN is the noun
+# parser_chunking = nltk.RegexpParser(grammar)
+#
+# parser_chunking.parse(sentence)
+#
+# output = parser_chunking.parse(sentence)
+#
+# output.draw()
 
-sentence = [("a", "DT"),("clever","JJ"),("fox","NN"),("was","VBP"),
-   ("jumping","VBP"),("over","IN"),("the","DT"),("wall","NN")]
 
-grammar = "NP:{<DT>?<JJ>*<NN>}"
+import spacy
+import ebooklib
+from ebooklib import epub
+# Spicy version, python version, en_core_web_sm version
+# # Load English tokenizer, tagger, parser and NER
+from MachineBookExtract.venv.epub2txt import main_method
 
-parser_chunking = nltk.RegexpParser(grammar)
+nlp = spacy.load("en_core_web_sm")
+print("Test1")
+# Process whole documents
+text = ("When Sebastian Thrun started working on self-driving cars at "
+        "Google in 2007, few people outside of the company took him "
+        "seriously. “I can tell you very senior CEOs of major American "
+        "car companies would shake my hand and turn away because I wasn’t "
+        "worth talking to,” said Thrun, in an interview with Recode earlier "
+        "this week.")
+doc = nlp(text)
 
-parser_chunking.parse(sentence)
+# Analyze syntax
+# print("Noun phrases:", [chunk.text for chunk in doc.noun_chunks])
+# print("Verbs:", [token.lemma_ for token in doc if token.pos_ == "VERB"])
+#
+# # Find named entities, phrases and concepts
+# for entity in doc.ents:
+#     print(entity.text, entity.label_)
 
-output = parser_chunking.parse(sentence)
+#book = epub.read_epub(r'C:\Users\pa-wo\Desktop\Studia\Magisterka\MachineBookExtract\MobyDick.epub')
+# str = []
 
-output.draw()
+#main_method(r'C:\Users\pa-wo\Desktop\Studia\Magisterka\MachineBookExtract\MobyDick.epub')
+
+str = open('test111.txt', 'r').read()
+
+#print(str)
+
+# text = ("When Sebastian Thrun started working on self-driving cars at "
+#         "Google in 2007, few people outside of the company took him "
+#         "seriously. “I can tell you very senior CEOs of major American "
+#         "car companies would shake my hand and turn away because I wasn’t "
+#         "worth talking to,” said Thrun, in an interview with Recode earlier "
+#         "this week.")
+
+info = (str[:975] + '..') if len(str) > 975 else str
+info = info.replace('\n', '')
+info = info.replace('        ## CHAPTER', '')
+info = info.replace('�', '')
+
+doc = nlp(info)
+
+# Analyze syntax
+print("Noun phrases:", [chunk.text for chunk in doc.noun_chunks])
+print("Verbs:", [token.lemma_ for token in doc if token.pos_ == "VERB"])
+#
+# Find named entities, phrases and concepts
+for entity in doc.ents:
+    print(entity.text, entity.label_)
+
