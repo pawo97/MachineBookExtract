c0577b45a8dee95ef5e79e4013760ca633e34055
p.wolski@polsource.com <pa-wo97@o2.pl>
2021-03-24 18:28:24 +0100

    Add English books

--- MachineBookExtract/Main.py
+++ MachineBookExtract/Main.py
@@ -1,7 +1,54 @@
-# import nltk
 
 # extract epub to text:
 # https://github.com/kevinxiong/epub2txt
+
+# import nltk
+# from nameparser.parser import HumanName
+#
+# def get_human_names(text):
+#     tokens = nltk.tokenize.word_tokenize(text)
+#     pos = nltk.pos_tag(tokens)
+#     sentt = nltk.ne_chunk(pos, binary = False)
+#     person_list = []
+#     person = []
+#     name = ""
+#     for subtree in sentt.subtrees(filter=lambda t: t.node == 'PERSON'):
+#         for leaf in subtree.leaves():
+#             person.append(leaf[0])
+#         if len(person) > 1: #avoid grabbing lone surnames
+#             for part in person:
+#                 name += part + ' '
+#             if name[:-1] not in person_list:
+#                 person_list.append(name[:-1])
+#             name = ''
+#         person = []
+#
+#     return (person_list)
+#
+# text = """
+# Some economists have responded positively to Bitcoin, including
+# Francois R. Velde, senior economist of the Federal Reserve in Chicago
+# who described it as "an elegant solution to the problem of creating a
+# digital currency." In November 2013 Richard Branson announced that
+# Virgin Galactic would accept Bitcoin as payment, saying that he had invested
+# in Bitcoin and found it "fascinating how a whole new global currency
+# has been created", encouraging others to also invest in Bitcoin.
+# Other economists commenting on Bitcoin have been critical.
+# Economist Paul Krugman has suggested that the structure of the currency
+# incentivizes hoarding and that its value derives from the expectation that
+# others will accept it as payment. Economist Larry Summers has expressed
+# a "wait and see" attitude when it comes to Bitcoin. Nick Colas, a market
+# strategist for ConvergEx Group, has remarked on the effect of increasing
+# use of Bitcoin and its restricted supply, noting, "When incremental
+# adoption meets relatively fixed supply, it should be no surprise that
+# prices go up. And that’s exactly what is happening to BTC prices."
+# """
+#
+# names = get_human_names(text)
+# print("LAST, FIRST")
+# for name in names:
+#         last_first = HumanName(name).last + ', ' + HumanName(name).first
+#         print(last_first)
 
 #
 # text = "Bed and chair are types of furniture"
@@ -18,6 +65,7 @@
 # #
 # # NN is the noun
 #
+# import nltk
 # sentence = [("a", "DT"),("clever","JJ"),("fox","NN"),("was","VBP"),
 #    ("jumping","VBP"),("over","IN"),("the","DT"),("wall","NN")]
 #
@@ -32,6 +80,8 @@
 # output.draw()
 
 
+##SPACY
+# from collections import Counter
 import spacy
 import ebooklib
 from ebooklib import epub
@@ -63,7 +113,7 @@ doc = nlp(text)
 
 #main_method(r'C:\Users\pa-wo\Desktop\Studia\Magisterka\MachineBookExtract\MobyDick.epub')
 
-str = open('test111.txt', 'r').read()
+str = open('test111.txt',  encoding="utf8").read()
 
 #print(str)
 
@@ -74,18 +124,40 @@ str = open('test111.txt', 'r').read()
 #         "worth talking to,” said Thrun, in an interview with Recode earlier "
 #         "this week.")
 
-info = (str[:975] + '..') if len(str) > 975 else str
-info = info.replace('\n', '')
-info = info.replace('        ## CHAPTER', '')
-info = info.replace('�', '')
+info = (str[:999990] + '..') if len(str) > 999990 else str
+# info = info.replace('\n', '')
+# info = info.replace('        ## CHAPTER', '')
+# info = info.replace('�', '')
+
+
+
+# info = '''Call me John. Some years ago never mind how long precisely having little or
+# no money in my purse, and nothing particular to interest me on shore, I
+# thought I would sail about a little and see the watery part of the world. It
+# is a way I have of driving off the spleen and regulating the circulation.
+# Whenever I find myself growing grim about the mouth; whenever it is a damp,
+# drizzly November in my soul; whenever I find myself involuntarily pausing
+# before coffin warehouses, and bringing up the rear of every funeral I meet;
+# and especially whenever my hypos get such an upper hand of me, that it
+# requires a strong moral principle to prevent me from deliberately stepping
+# into the street, and methodically knocking people's hats off then, I account
+# it high time to get to sea as soon as I can. This is my substitute for pistol
+# and ball. With a philosophical flourish Cato throws himself upon his sword; I
+# quietly take to the ship. There is nothing surprising in this. If they but
+# knew it, almost all men in their degree, some time or other, cherish very
+# nearly the same feelings towards the ocean with me.'''
 
 doc = nlp(info)
 
 # Analyze syntax
 print("Noun phrases:", [chunk.text for chunk in doc.noun_chunks])
 print("Verbs:", [token.lemma_ for token in doc if token.pos_ == "VERB"])
-#
+
 # Find named entities, phrases and concepts
+
+
 for entity in doc.ents:
-    print(entity.text, entity.label_)
+        #if entity.label_ == 'PERSON':
+        #print(entity.text)
+        print(entity.text, entity.label_)
 
